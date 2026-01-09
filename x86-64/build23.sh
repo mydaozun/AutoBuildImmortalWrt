9c2674223ace75dac992fc2c792732163f74c121
daozun <36945443+mydaozun@users.noreply.github.com>
2026-01-09 15:15:19 +0800

    Update build23.sh
    # 解锁网易云灰色歌曲(推荐)
    PACKAGES="$PACKAGES luci-app-unblockneteasemusic"
    # iStore首页和网络向导(推荐)
    # PACKAGES="$PACKAGES luci-app-quickstart"
    # iStore首页和网络向导
    CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
    # 高级卸载 by YT Vedio Talk
    CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
    # 应用过滤 openappfilter.com
    CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-appfilter luci-i18n-appfilter-zh-cn"
    # Bandix流量监控 by timsaya
    CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-bandix luci-i18n-bandix-zh-cn"

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -59,6 +59,20 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+
+# 解锁网易云灰色歌曲(推荐)
+PACKAGES="$PACKAGES luci-app-unblockneteasemusic"
+# iStore首页和网络向导(推荐)
+# PACKAGES="$PACKAGES luci-app-quickstart"
+# iStore首页和网络向导
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-quickstart-zh-cn"
+# 高级卸载 by YT Vedio Talk
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
+# 应用过滤 openappfilter.com
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-appfilter luci-i18n-appfilter-zh-cn"
+# Bandix流量监控 by timsaya
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-bandix luci-i18n-bandix-zh-cn"
+
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
