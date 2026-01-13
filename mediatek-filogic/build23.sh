13c26b0c1261864019d3dfc6b38eeb788050b6ef
daozun <36945443+mydaozun@users.noreply.github.com>
2026-01-13 13:56:11 +0800

    Update build23.sh

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -75,7 +75,7 @@ PACKAGES="$PACKAGES luci-i18n-quickstart-zh-cn"
 PACKAGES="$PACKAGES luci-app-uninstall"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-uninstall"
 # 进阶设置 by sirpdboy 
-PACKAGES="$PACKAGES luci-i18n-advancedplus-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-advancedplus-zh-cn"
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-advancedplus luci-i18n-advancedplus-zh-cn"
 
 # 增加几个必备组件 方便用户安装iStore
