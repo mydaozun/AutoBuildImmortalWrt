f6dc38b2e94d7e05dde14096495b0112ba63f96a
daozun <36945443+mydaozun@users.noreply.github.com>
2026-01-13 09:37:15 +0800

    Disable argon theme package installations
    Comment out argon theme packages in build script

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -57,8 +57,8 @@ PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # argon主题
 PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+#PACKAGES="$PACKAGES luci-app-argon-config"
+#PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 
 
 # PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
